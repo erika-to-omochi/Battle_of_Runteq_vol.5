d006c6fe7edba463a4d31b4fded4a74ade23304c
erika komori <komoriakirarika@erikaMBA.local>
2024-06-01 15:51:40 +0900

    “add_flash_messagre”

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def user_logged_in?
       logged_in?
     end
+
+    def flash_background_color(type)
+      case type.to_sym
+      when :notice 
+        "bg-blue-300 border-l-4 border-blue-500 text-blue-700 p-4"
+      when :alert  
+        "bg-red-500 border-l-4 border-red-700 text-red-100 p-4"
+      else "bg-gray-500"
+      end
+    end
   end

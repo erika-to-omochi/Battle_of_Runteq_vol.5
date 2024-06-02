a3e279a8d72b04da676d174665db1a57165ebf58
erika komori <komoriakirarika@erikaMBA.local>
2024-06-02 13:31:48 +0900

    add_phinajs_controller

--- app/controllers/phina_controller.rb
+++ app/controllers/phina_controller.rb
@@ -1,6 +1,5 @@
 class PhinaController < ApplicationController
   skip_before_action :require_login
   def js
-    send_file Rails.root.join('app', 'assets', 'javascripts', 'phina.js'), type: 'application/javascript', disposition: 'inline'
   end
 end

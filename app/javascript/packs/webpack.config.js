0c224a59d696e97b003d0c6d4e6ed14d5a5e6c63
erika komori <komoriakirarika@erikaMBA.local>
2024-06-03 09:25:54 +0900

    add_webpack.config.js

---
+++ app/javascript/packs/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+module.exports = {
+    // 他の設定
+    node: false
+  };

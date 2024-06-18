7b6e3ee6c9c9627015d6fa6f76f54f5d8e932f7b
erika komori <komoriakirarika@erikaMBA.local>
2024-06-18 13:21:29 +0900

    login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,11 +4,14 @@ class UserSessionsController < ApplicationController
   def new; end
 
   def create
+    logger.debug "Login attempt with email: #{params[:email]}"
     @user = login(params[:email], params[:password])
 
     if @user
+      logger.debug "Login successful for user: #{@user.id}"
       redirect_to root_path, notice: 'ログインしました'
     else
+      logger.debug "Login failed for email: #{params[:email]}"
       flash.now[:alert] = 'ログインに失敗しました'
       render :new, status: :unprocessable_entity
     end
@@ -17,6 +20,5 @@ class UserSessionsController < ApplicationController
   def destroy
     logout
     redirect_to root_path, notice: 'ログアウトしました', status: :see_other
-
   end
 end

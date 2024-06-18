256b8e710a019bf409e8c56abc6bf5df7dd7136f
erika komori <komoriakirarika@erikaMBA.local>
2024-06-18 14:38:59 +0900

    login

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,7 +23,7 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.find(params[:id])
     @comment.destroy!
     respond_to do |format|
-      format.turbo_stream
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@comment) }
       format.html { redirect_to board_path(@comment.board), notice: 'コメントが削除されました。' }
     end
   end

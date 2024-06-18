a4838d930cb821ac1cf687f9f86cb528fe4764f5
erika komori <komoriakirarika@erikaMBA.local>
2024-06-18 10:22:10 +0900

    game

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'phina/js'
   get 'build/phina.js', to: 'phina#js'
   get 'games/shooting'
+  get 'games/prologue'
   resources :users, only: %i[index new create]
   resources :boards, only: %i[index new create show edit update destroy] do
     resources :comments, only: %i[create new show edit destroy update index], shallow: true

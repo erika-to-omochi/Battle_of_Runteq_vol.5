889c0a58f3205f34fa62c6b57b80bba6c388484b
erika komori <komoriakirarika@erikaMBA.local>
2024-06-18 13:47:31 +0900

    login

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,15 @@
 class GamesController < ApplicationController
-  skip_before_action :require_login
+  before_action :set_phina_js, only: [:shooting, :prologue]
+
   def shooting
   end
 
   def prologue
+  end
+
+  private
+
+  def set_phina_js
+    @use_phina_js = true
   end
 end
